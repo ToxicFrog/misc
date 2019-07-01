7a801895ea4ca7c5b001687c62b59d47bef57d01
B. Kelly <bk@ancilla.ca>
2019-07-01 09:26:49 -0400

    netstat

---
+++ bitburner/bin/netstat.lua
@@ -0,0 +1,20 @@
+-- Display a tree of network information.
+
+local net = require 'net'
+
+local maxdepth = tonumber((...)) or 5
+
+local function showHost(host, depth)
+  local stat = net.stat(host)
+  printf("%-24.24s  %d %4d %s  %3.0f/%-3.0f  (%0.2f) %18s",
+    (" "):rep(depth) ..  host,
+    stat.ports, stat.hack_level, stat.root and "R" or " ",
+    stat.security, stat.min_security,
+    stat.money/(stat.max_money > 0 and stat.max_money or 1), -- makes 0/0 show up as 0 rather than inf
+    tomoney(stat.money));
+  return depth < maxdepth
+end
+
+printf("%24s  %s %s %s  %3s/%-3s  %s %15s",
+  "hostname", "P", "Hack", "R", "Sec", "Min", "$Ratio", "$Total");
+net.walk(showHost, ns:getHostname())
---
+++ bitburner/lib/net.lua
@@ -0,0 +1,42 @@
+-- Utility library for getting information about the network and hosts on it.
+
+local net = {}
+
+-- helper for net.walk
+local function walkOne(fn, host, depth, seen)
+  seen[host] = true
+  if not fn(host, depth) then return end
+  for peer in js.of(ns:scan(host)) do
+    if not seen[peer] then
+      walkOne(fn, peer, depth+1, seen)
+    end
+  end
+end
+
+-- Traverse the net, starting at root, and call fn(hostname, depth) on each
+-- host reachable from it. Depth is the number of hops away from root the given
+-- host is; it is 0 for root.
+-- The fn should return true if traversal should continue through that host,
+-- false to stop traversal.
+function net.walk(fn, root)
+  return walkOne(fn, root, 0, {})
+end
+
+-- Scan a single host and return information about it.
+function net.stat(host)
+  local stat = {}
+  stat.name = host
+  stat.root = ns:hasRootAccess(host)
+  stat.ports = ns:getServerNumPortsRequired(host)
+  stat.ram = ns:getServerRam(host)[0]
+  stat.ram_used = ns:getServerRam(host)[1]
+  stat.security = ns:getServerSecurityLevel(host)
+  stat.min_security = ns:getServerMinSecurityLevel(host)
+  stat.money = ns:getServerMoneyAvailable(host)
+  stat.max_money = ns:getServerMaxMoney(host)
+  stat.hack_level = ns:getServerRequiredHackingLevel(host)
+  stat.hack_fraction = ns:hackAnalyzePercent(host)/100
+  return stat
+end
+
+return net
