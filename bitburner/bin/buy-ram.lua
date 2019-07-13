f84d8916883fe3f7a3a30de68a2720247717461e
B. Kelly <bk@ancilla.ca>
2019-07-12 22:11:29 -0400

    keep upgrading home system RAM

--- bitburner/bin/buy-ram.lua
+++ bitburner/bin/buy-ram.lua
@@ -4,12 +4,14 @@ local MAX_RAM = 256
 local TIME_BETWEEN_BUYS = 5.0
 
 while true do
-  if ns:getServerRam('home')[0] < MAX_RAM then
-    local money = ns:getServerMoneyAvailable 'home'
-    local cost = ns:getUpgradeHomeRamCost()
-    if cost < money then
-      ns:upgradeHomeRam()
-    end
+  local money = ns:getServerMoneyAvailable 'home'
+  if ns:getServerRam('home')[0] >= MAX_RAM then
+    -- If we've hit our RAM target, only upgrade when it's cheap to do so.
+    money = money * 0.1
+  end
+  local cost = ns:getUpgradeHomeRamCost()
+  if cost <= money then
+    ns:upgradeHomeRam()
   end
   ns:sleep(TIME_BETWEEN_BUYS)
 end
