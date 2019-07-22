81c02fb7ada18804774b8fecc6e72ac343277224
B. Kelly <bk@ancilla.ca>
2019-07-22 13:30:14 -0400

    disable watchdog for cct solver

--- bitburner/bin/cct-solver.lua
+++ bitburner/bin/cct-solver.lua
@@ -6,6 +6,10 @@ local TIME_BETWEEN_SCANS = 60*60
 local cct = require 'cct'
 local net = require 'net'
 
+-- Disable the watchdog timer, since it interferes with the coroutines used
+-- by some of the solvers.
+function atwatchdog() end
+
 local function solveCCTs(host, depth)
   for file in js.of(ns:ls(host)) do
     if file:match("%.cct$") then
