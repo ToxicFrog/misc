efd93aa49c10c72f138b94609fb373d2fc832dd5
B. Kelly <bk@ancilla.ca>
2020-04-24 23:16:48 -0400

    gbf-pc: add some new selectors and shortcuts

--- gbf-pc.user.js
+++ gbf-pc.user.js
@@ -74,7 +74,7 @@ function NumberButtonSelector(n) {
       // Ability selection
       `div.btn-ability-available:nth-child(${n})`,
       // Summon selection, once the summon panel has been opened
-      `div.lis-summon.on[pos=${n}]`, // or .summon-available instead of .on, not sure which is better
+      `div.prt-summon-list.opened > div.lis-summon.on[pos=${n}]`, // or .summon-available instead of .on, not sure which is better
     ),
   ];
 }
@@ -88,13 +88,26 @@ function NumberButtonSelector(n) {
 // - if a list, each element is evaluated in order using these rules; evaluation stops when
 //   one element succeeds
 // In any case, if the selector evaluates to exactly one element, that element is activated.
+
+// Selectors for dialogue or splash screens where advancing them is "safe", i.e.
+// you won't be inadvertently agreeing to or cancelling something.
+let dialogueOrSplashScreen = AnyOf(
+  'div.prt-advice', 'div.prt-scene-comment', 'div.btn-result',
+  'canvas#cjs-gacha', 'canvas#cjs-login', 'canvas#cjs-panel-mission',
+  //'div.cnt-quest',
+);
 let shortcuts = {
-  // a => attack; can also use Enter
-  'a': 'div.btn-attack-start.display-on',
   // s, h, c for summons, heal, and CA toggle
-  's': 'div.btn-command-summon.summon-on',
+  's': 'div.prt-summon-list > div.prt-list-top.btn-command-summon',
   'h': 'div.prt-sub-command div.btn-temporary',
   'c': 'div.prt-sub-command div.btn-lock',
+  // QAZ to target enemies
+  'q': 'a.btn-targeting.enemy-1',
+  // This isn't a typo; enemies 1 and 2 are always the top and bottom ones respectively,
+  // so if you're fighting 3 enemies, #3 goes in the middle.
+  // 'a' can also be used to attack, or autoselect in the upgrade screen.
+  'a': AnyOf('a.btn-targeting.enemy-3', 'div.btn-attack-start.display-on', 'div.btn-recommend'),
+  'z': 'a.btn-targeting.enemy-2',
   // numbers => character, ability, or summon selection
   '1': NumberButtonSelector(1),
   '2': NumberButtonSelector(2),
@@ -103,7 +116,7 @@ let shortcuts = {
   '5': NumberButtonSelector(5), // TODO perhaps should also open summon menu
   '6': NumberButtonSelector(6),
   // Arrows for next/prev
-  'ArrowLeft': 'div.ico-pre',
+  'ArrowLeft': 'div.ico-pre', // btn-prev-job btn-next-job
   'ArrowRight': 'div.ico-next',
   // Enter activates ok/next/quest buttons, attacks in combat, or steps through dialogue or in-battle hints
   // If a popup is open it prefers buttons in the popup
@@ -112,16 +125,18 @@ let shortcuts = {
     'div.pop-usual div.btn-usual-ok',
     'div.pop-usual div.btn-usual-close',
     // Otherwise step through dialogue
-    AnyOf('div.prt-advice', 'div.prt-scene-comment'),
+    dialogueOrSplashScreen,
     // If no popup or dialogue, look for any sort of OK/next screen/attack button and activate it.
-    // TODO: investigate other stuff in .prt-button-area: .btn-{unclaimed,retry,continue,retry-sequence}
-    AnyOf('div.btn-usual-ok', 'div.btn-usual-close', 'div.btn-result',
+    AnyOf('div.btn-usual-ok', 'div.btn-usual-close',
+          'div.btn-lupi.multi', // 10 part rupie gacha
+          'div.btn-synthesis', 'div.btn-evolution', // upgrade and uncap
+          // TODO: investigate other stuff in .prt-button-area:
+          //.btn-{unclaimed,retry,continue,retry-sequence}
           'div.prt-button-area.upper div.btn-control',
           'div.btn-attack-start.display-on'),
-    // 'div.cnt-quest', // the "new area discovered" popup
   ],
   // Spacebar advances dialogue but doesn't activate buttons or attack.
-  ' ': AnyOf('div.prt-advice', 'div.prt-scene-comment', 'div.btn-result'),
+  ' ': dialogueOrSplashScreen,
   // Escape and Backspace activate cancel/back buttons, same popup behaviour as Enter.
   'Escape': [
     Cond('div.pop-usual', ['div.btn-command-back.display-on', 'div.btn-usual-cancel', 'div.btn-usual-close']),
@@ -130,6 +145,7 @@ let shortcuts = {
   'Backspace': [
     Cond('div.pop-usual', ['div.btn-command-back.display-on', 'div.btn-usual-cancel', 'div.btn-usual-close']),
     AnyOf('div.btn-command-back.display-on', 'div.btn-usual-cancel'),
+    'div.lis-lead-prev',
   ],
   'alt-w': Go('quest/island'),
   'alt-q': Go('quest'),
@@ -144,6 +160,7 @@ let shortcuts = {
 
 // Given a selector or selector tree from the keymap, try to turn it into a control and activate it
 function trySelector(selector) {
+  // console.info("trySelector:", selector);
   if (typeof(selector) == "string") {
     if (selector.indexOf(',') == -1) {
       // Single selector
@@ -173,9 +190,23 @@ function tap(selector) {
     if (elems.length > 1) console.info("Warning:", elems.length, "elements returned from selector", selector);
     return false;
   }
-  let evt = document.createEvent('Events');
-  evt.initEvent('tap', true, false);
-  elems[0].dispatchEvent(evt);
+  let elem = elems[0];
+  if (elem.tagName == "CANVAS") {
+    // Special handling for <canvas> tags -- we can't just send them a "tap" event,
+    // we need to actually send separate "touchstart" and "touchend" events with
+    // valid coordinates inside the canvas's bounding rect.
+    let r = elem.getBoundingClientRect();
+    let t = new Touch({
+      identifier: 1, target: elem,
+      force: 1, radiusX: 1, radiusY: 1,
+      pageX: r.x + r.width/2, pageY: r.y + r.height/2,
+    })
+    let options = {bubbles: true, changedTouches: [t], targetTouches: [t], touches: [t]};
+    elem.dispatchEvent(new TouchEvent("touchstart", options));
+    elem.dispatchEvent(new TouchEvent("touchend", options));
+  } else {
+    elem.dispatchEvent(new UIEvent("tap", {bubbles: true}));
+  }
   return true;
 }
 
