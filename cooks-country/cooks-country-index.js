56655818f145f5fd8c075f8472db7fb0d541df7f
Rebecca Kelly <bk@ancilla.ca>
2020-09-23 21:33:10 -0400

    fixes

--- cooks-country/cooks-country-index.js
+++ cooks-country/cooks-country-index.js
@@ -1,8 +1,15 @@
 const ETCD = '/v3/kv/';
 
+// function asciiOnly(key) {
+//   return key.replace(/[\u00A0-\uFFFF]/g,
+//     ch => '\\\\u{' + ch.codePointAt(0).toString(16) + '}');
+// }
+
 /* Fetch an item from the remote K-V store, return a promise that resolves to
    the value if present or null otherwise */
 function getItem(key) {
+  key = encodeURI(key);
+  // console.log('getItem', key);
   return fetch(ETCD+'range', {
     credentials: 'same-origin',
     method: 'POST',
@@ -15,67 +22,128 @@ function getItem(key) {
     return response.json()
   }).then(function(json) {
     if (!json || !json.kvs || !json.kvs[0].value) {
-      console.log("getItem", key, "null");
       return null;
     }
-    console.log("getItem", key, atob(json.kvs[0].value));
-    return atob(json.kvs[0].value);
-  })
+    return JSON.parse(decodeURI(atob(json.kvs[0].value)));
+  }).catch(console.log);
 }
 
 /* Set an item in the K-V store, return a promise that resolves to the server response */
 function setItem(key, value) {
-  console.log("setItem", key, value);
+  // console.log('putItem', key, value);
   return fetch(ETCD+'put', {
     credentials: 'same-origin',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({key: btoa(key), value: btoa(value)}),
+    body: JSON.stringify({
+      key: btoa(encodeURI(key)),
+      value: btoa(encodeURI(JSON.stringify(value)))
+    }),
   }).then(function(response) {
     if (!response.ok) return Promise(null);
     return response.json();
-  })
+  }).catch(console.log);
 }
 
 function restoreState(recipe, bit, value) {
   let cb = document.getElementById('cb:'+bit+':'+recipe)
   cb.checked = !!value;
-  cb.onchange();
+  findParent(cb, 'TR').classList.toggle(bit, !!value);
+}
+
+function findParent(self, tag) {
+  while (self.tagName != tag) {
+    self = self.parentElement;
+  }
+  return self;
 }
 
 function toggleState(self, bit) {
-  // Find the enclosing <tr>
   let checked = self.checked;
-  while (self.tagName != "TR") {
-    self = self.parentElement;
-  }
+  self = findParent(self, 'TR');
   let recipe = self.id;
   if (checked) {
-    setItem(recipe + "/" + bit, "yes")
+    // setItem(recipe + "/" + bit, "yes");
     self.classList.add(bit);
   } else {
-    setItem(recipe + "/" + bit, "");
+    // setItem(recipe + "/" + bit, "");
     self.classList.remove(bit);
   }
+  let issue = findParent(self, 'TABLE').id;
+  setItem(issue, buildIssueJson(issue));
 }
 
 function restoreAll(_) {
+  let tables = document.getElementsByTagName('table');
+  let n=0;
+  for (table of tables) {
+    let issue = table.id;
+    setTimeout(_ => getItem(issue).then(applyIssueJson), n*100);
+    n++;
+  }
+  setTimeout(_ => { document.getElementById('loading').classList.add('loading-done'); }, n*100);
+}
+
+function restoreAllOld(_) {
   let rows = document.querySelectorAll('tr.recipe-row');
+  let n=0;
   for (let row of rows) {
     let recipe = row.id;
-    getItem(recipe+'/written').then(v => restoreState(recipe, 'written', v));
-    getItem(recipe+'/cooked').then(v => restoreState(recipe, 'cooked', v));
-    getItem(recipe+'/hidden').then(v => restoreState(recipe, 'hidden', v));
+    setTimeout(_ => {
+      getItem(recipe+'/written').then(v => restoreState(recipe, 'written', v));
+      getItem(recipe+'/cooked').then(v => restoreState(recipe, 'cooked', v));
+      getItem(recipe+'/hidden').then(v => restoreState(recipe, 'hidden', v));
+    }, n*100);
+    ++n;
   }
+  setTimeout(_ => { document.getElementById('loading').classList.add('loading-done'); }, n*100);
 }
 
 function toggleStar(self) {
   if (self.innerText == "★") {
     self.innerText = "☆";
+    findParent(self, 'TR').classList.remove('starred');
   } else {
     self.innerText = "★";
+    findParent(self, 'TR').classList.add('starred');
+  }
+}
+
+function toggleRowFilter(filter) {
+  for (let tr of document.getElementsByTagName('tr')) {
+    tr.classList.toggle(filter);
+  }
+}
+
+function buildIssueJson(issue) {
+  let json = {};
+  let rows = document.querySelectorAll('tr[id*="'+issue+'/"]');
+  for (let row of rows) {
+    let id = row.id;
+    let recipe = {};
+    recipe.cooked = document.getElementById('cb:cooked:'+id).checked;
+    recipe.written = document.getElementById('cb:written:'+id).checked;
+    recipe.hidden = document.getElementById('cb:hidden:'+id).checked;
+    json[id] = recipe;
+  }
+  return json;
+}
+
+function applyIssueJson(json) {
+  for (let id in json) {
+    let recipe = json[id];
+    for (let bit of ['cooked', 'written', 'hidden']) {
+      let cb = document.getElementById('cb:'+bit+':'+id);
+      let row = document.getElementById(id);
+      if (!cb || !row) {
+        console.log("Warning: couldn't find elements for recipe: ", id);
+        continue;
+      }
+      cb.checked = recipe[bit];
+      row.classList.toggle(bit, recipe[bit]);
+    }
   }
 }
 
