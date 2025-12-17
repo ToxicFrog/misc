bb4327817dd4322db2a61a555dad83175af67f07
Rebecca Kelly <bk@ancilla.ca>
2025-12-17 08:00:36 -0500

    Add the rest of Iron Maiden B1

--- iron-maiden-b1.lua
+++ iron-maiden-b1.lua
@@ -20,7 +20,6 @@ region 'Iron Maiden B1' {
       'Mandrake Sigil', 'Grimoire Exsorcer'
     };
   };
-  -- TODO: rest of B1
   room 'Wooden Horse' {
     n 'The Cauldron';
     s 'Starvation';
@@ -28,6 +27,14 @@ region 'Iron Maiden B1' {
   room 'Starvation' {
     n 'Wooden Horse';
     s 'The Breast Ripper';
+    enemy 'Wraith' {
+      miniboss = true;
+      'Kalmia Sigil', 'Grimoire Venin'
+    };
+    enemy 'Mummy' {
+      miniboss = true;
+      'Shamshir.H/Knuckle Guard', 'Vera Bulb x3'
+    };
   };
   room 'The Breast Ripper' {
     n 'Starvation';
@@ -37,37 +44,74 @@ region 'Iron Maiden B1' {
   };
   room 'The Wheel' {
     w 'The Breast Ripper';
+    enemy 'Dark Skeleton';
+    enemy 'Shadow';
+    chest {
+      warded = true;
+      'Bull Shot:Griever.H/Bhuj Type', 'Baselard.H',
+      'Djinn Amber', 'Valens'
+    };
   };
   room 'The Branks' {
     e 'The Breast Ripper';
+    enemy 'Dark Skeleton';
+    enemy 'Shadow';
+    chest {
+      locked = true;
+      'Balalaika:Double Blade.H/Bhuj Type',
+      'Bec de Corbin.H', 'Dao Moonstone', 'Volare'
+    };
   };
   room 'The Pear' {
+    -- TODO: the jump to get back out of here is an absolute bastard
+    -- without faerie wing. We may want to require that before this
+    -- is in logic, either in the pool or via access to snowfly forest.
+    -- alternately, if we have a Super Metroid style teleport-to-save
+    -- command, that guarantees we can't get stuck.
     n 'The Breast Ripper';
     e 'The Judas Cradle';
     w 'The Whirlygig';
   };
   room 'The Judas Cradle' {
     w 'The Pear';
+    enemy 'Dark Skeleton';
+    chest {
+      'Sonora:Bastard Sword.H/Power Palm', 'Bullova.H',
+      'Ifrit Carnelian', 'Prudens'
+    };
   };
   room 'The Whirlygig' {
     e 'The Pear'; dx=-1;
     n 'Spanish Tickler';
+    enemy 'Dark Skeleton';
   };
   room 'Spanish Tickler' {
     s 'The Whirlygig';
     n 'Heretic\'s Fork';
+    enemy 'Wyvern Knight' {
+      miniboss = true;
+      'Elixir of Dragoons', 'Elixir of Queens', 'Chest Key'
+    };
   };
   room 'Heretic\'s Fork' {
     s 'Spanish Tickler';
     e 'The Chair of Spikes';
+    trap 'Freeze';
+    trap 'Gust';
+    enemy 'Dark Skeleton';
   };
   room 'The Chair of Spikes' {
     w 'Heretic\'s Fork';
     e 'Blooding';
+    enemy 'Dark Skeleton';
+    enemy 'Wraith';
   };
   room 'Blooding' {
     w 'The Chair of Spikes'; dx=2;
     s 'Bootikens';
+    trap 'Death Vapor';
+    trap 'Eruption';
+    enemy 'Dark Skeleton';
   };
   room 'Bootikens' {
     n 'Blooding';
@@ -76,18 +120,32 @@ region 'Iron Maiden B1' {
   room 'Burial' {
     n 'Bootikens';
     s 'Burning';
+    enemy 'Iron Golem' {
+      boss = true;
+      'Columbine Sigil', 'Elixir of Dragoons'
+    }
   };
   room 'Burning' {
     n 'Burial'; dy=-1;
     w 'Cleansing the Soul';
+    trap 'Terra Thrust';
+    trap 'Holy Light';
   };
   room 'Cleansing the Soul' {
     e 'Burning';
     w 'The Ducking Stool';
     n 'The Garotte';
+    enemy 'Dark Skeleton';
+    enemy 'Wraith';
   };
   room 'The Ducking Stool' {
     e 'Cleansing the Soul';
+    enemy 'Dark Skeleton';
+    enemy 'Shadow';
+    chest {
+      'Red Viking:Khora.H/Power Palm', 'Pole Axe.H',
+      'Marid Aquamarine', 'Virtus'
+    };
   };
   room 'The Garotte' {
     s 'Cleansing the Soul';
@@ -96,6 +154,8 @@ region 'Iron Maiden B1' {
   room 'Hanging' {
     e 'The Garotte'; dx=-1;
     s 'Impalement' 'Steel Key';
+    enemy 'Dark Skeleton';
+    enemy 'Wraith';
   };
   room 'Impalement' {
     n 'Hanging';
@@ -103,6 +163,10 @@ region 'Iron Maiden B1' {
   };
   room 'Knotting' {
     n 'Impalement';
-    --s -- to Iron Maiden B2
+    s 'The Eunics\' Lot'; -- to Iron Maiden B2
+    enemy 'Wyvern Queen' {
+      miniboss = true;
+      'Anemone Sigil', 'Elixir of Sages'
+    };
   };
 }
